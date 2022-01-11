7c18d641db743d5d7fe510476edddeefed01de26
emre topcu <emre.topcu1234@gmail.com>
2022-01-12 00:37:16 +0300

    Initial Commit

---
+++ MyChat/MyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatApp.swift
+//  MyChat
+//
+//  Created by Emre Topçu on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
